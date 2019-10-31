1f8a192fb495965dfe9f91c4a85d2fc6467e7b68
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-10-31 15:38:17 -0700

    MVP

--- middleware/validateUser.js
+++ middleware/validateUser.js
@@ -2,9 +2,22 @@ module.exports = validateUser;
 
 function validateUser(req, res, next) {
   const validUser = req.body;
-  if (!validUser.name) {
-    res.status(400).json({message: "Missing required name field." })
-    return
+  if(!validUser) {
+    res.status(400).json({message: "No User data provided." }) 
+  } else if (!validUser.name) {
+    res.status(400).json({message: "Missing required name field." }) 
+  } else {
+    Users.get()
+    .then(users => {
+      if (users.find((item) => item.name === req.body.name)) {
+        return res.status(400).json({error: "User with this name already exists."})
+      }
+      next();
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: "Internal server error.", source: "Validate User Middleware"})
+    })
   }
-  next();
+
 }
